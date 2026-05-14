32c34b8f586fcf774bda1dece8e9321fdc79b1dd
YouCanCode.net <hardpress.dev@proton.me>
2026-05-14 01:20:31 +0000

    Clean: Finalized site and base routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
+  // Your root domain
   site: 'https://youcancode.net',
+  // The subfolder for this specific project
   base: '/artist-demo-site/',
+  // Keeping the integrations the template needs to look good
   integrations: [tailwind(), sitemap()],
 });
