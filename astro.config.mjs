e03dd9e058b48cd1ea32305da2328f263ca7118d
YouCanCode.net <hardpress.dev@proton.me>
2026-05-14 01:22:40 +0000

    Done

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
-export default defineConfig({
-  // Your root domain
-  site: 'https://youcancode.net',
-  // The subfolder for this specific project
-  base: '/artist-demo-site/',
-  // Keeping the integrations the template needs to look good
-  integrations: [tailwind(), sitemap()],
-});
+// https://astro.build/config
+export default defineConfig({});
