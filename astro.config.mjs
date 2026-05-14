8e4177deb99d02d0c90c430495b4c7b911a7fd32
YouCanCode.net <hardpress.dev@proton.me>
2026-05-14 01:22:51 +0000

    Final Fix: Minimal config for Brian Alcaraz template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Your root custom domain
+  site: 'https://youcancode.net',
+  // Your repo-specific subpath
+  base: '/artist-demo-site/',
+});
