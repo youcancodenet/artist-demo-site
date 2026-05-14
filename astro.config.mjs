856270d8aee6711546c444ea688b8f23f211df16
YouCanCode.net <hardpress.dev@proton.me>
2026-05-14 01:16:22 +0000

    Clean install of professional-card-astro template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: 'https://youcancode.net',
   base: '/artist-demo-site/',
+  integrations: [tailwind(), sitemap()],
 });
